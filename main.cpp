7d5b1e97b48ce959ceefb2da41f9ceecb4efd3c8
marybadalyan <marybadalyangdh@gmail.com>
2025-04-18 20:46:21 +0400

    Remove accumulation

--- main.cpp
+++ main.cpp
@@ -5,16 +5,36 @@
     #include <windows.h>
     #include <processthreadsapi.h>
 #else
-    #include <stdio.h>
-    #include <stdlib.h>
     #include <signal.h>
-    #include <malloc.h>
-    #include <unistd.h> //rw
-    #include <cstring> // for strlen
-    #include <sys/resource.h>   
-
+    #include <sys/resource.h>
+    #include <unistd.h>
 #endif
 
+bool testArraySize(size_t sizeBytes) {
+    const size_t count = sizeBytes / sizeof(int); // Bytes to number of ints
+
+    #ifdef _WIN32
+        __try {
+            volatile int* arr = (int*)_malloca(count * sizeof(int));
+            arr[0] = 1;
+            arr[count - 1] = 1;
+            std::cout << "Allocated " << sizeBytes << " bytes" << std::endl;
+            return true;
+        }
+        __except (GetExceptionCode() == EXCEPTION_STACK_OVERFLOW ? 
+                EXCEPTION_EXECUTE_HANDLER : EXCEPTION_CONTINUE_SEARCH) {
+            std::cout << "Stack overflow at " << sizeBytes << " bytes!" << std::endl;
+            return false;
+        }
+    #else
+        volatile int arr[count];
+        arr[0] = 1;
+        arr[count - 1] = 1;
+
+        std::cout << "Allocated " << sizeBytes << " bytes" << std::endl;
+        return true;
+    #endif
+}
 
 #ifndef _WIN32
 void setup_segfault_handler() {
@@ -22,71 +42,38 @@ void setup_segfault_handler() {
     struct rlimit core_limit = {0, 0};
     setrlimit(RLIMIT_CORE, &core_limit);
 
-    // Set up signal handler for segmentation fault
+    // Set up signal handler
     struct sigaction sa;
     sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = [](int, siginfo_t*, void*) {
+    sa.sa_sigaction = [](int sig, siginfo_t*, void*) {
         const char* msg = "Segmentation fault detected (probably a stack overflow).\n";
         write(STDERR_FILENO, msg, strlen(msg)); // Safe in signal handler
-        _exit(EXIT_FAILURE); // Do not return from signal handler
+        _exit(EXIT_FAILURE); // use _exit to avoid flushing corrupted stack
     };
     sigemptyset(&sa.sa_mask);
     sigaction(SIGSEGV, &sa, nullptr);
 }
 #endif
 
-
-bool testArraySize(size_t sizeBytes) {
-    const size_t count = sizeBytes / sizeof(int); // Bytes to number of ints
-
+int StackSize() {
     #ifdef _WIN32
-        __try { // Structured Exception Handling
-            volatile int* arr = (int*)_malloca(count * sizeof(int));
-            arr[0] = 1;  
-            arr[count - 1] = 1;  
-            std::cout << "Allocated " << sizeBytes << " bytes" << std::endl;
-            return true;
-        }
-        __except (GetExceptionCode() == EXCEPTION_STACK_OVERFLOW ? 
-                EXCEPTION_EXECUTE_HANDLER : EXCEPTION_CONTINUE_SEARCH) { 
-                    std::cout << "Stack overflow at " << sizeBytes << " bytes!" << std::endl;
-                    return false; // Signal failure to stop loop
-        }
-    #else
-        // Allocate memory and check for stack overflow
-        volatile int arr[count]; // Local array on the stack
-        arr[0] = 1;
-        arr[count - 1] = 1;
-
-        totalAllocated += sizeBytes; // Track total allocated bytes
-
-        std::cout << "Allocated " << sizeBytes << " bytes" << std::endl;
-        return true;
-    #endif
-}
-
-
-int StackSize(){
-    #ifdef _WIN32
-        // Thread Information Block
         NT_TIB* tib = (NT_TIB*)NtCurrentTeb();
         SIZE_T stackSize = (SIZE_T)tib->StackBase - (SIZE_T)tib->StackLimit;
         std::cout << "Stack size (Windows): " << stackSize / 1024 << " KB\n";
-        return stackSize;
+        return static_cast<int>(stackSize);
     #else
         struct rlimit rl;
         if (getrlimit(RLIMIT_STACK, &rl) == 0) {
             std::cout << "Stack size (Linux): " << (rl.rlim_cur / (1024*1024)) << " KB\n";
-            return rl.rlim_cur;
+            return static_cast<int>(rl.rlim_cur);
         }
     #endif
-
     return 0;
 }
 
 int main() {
     #ifndef _WIN32
-    setup_segfault_handler(); // Linux only
+        setup_segfault_handler();
     #endif
 
     size_t startSize = 100;  
@@ -99,9 +86,8 @@ int main() {
             std::cout << "Program terminated due to stack overflow." << std::endl;
             break;
         }
-        
-        sizeBytes = static_cast<size_t>(sizeBytes * 1.5); 
+        sizeBytes = static_cast<size_t>(sizeBytes * 1.5); // Increase by 1.5x
     }
-    
+
     return 0;
 }
